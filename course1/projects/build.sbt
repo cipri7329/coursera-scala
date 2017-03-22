4697bdf6f3c22cbe1a361baf61724be068a1eb54
Ciprian Lucaci <Ciprian.Lucaci@ness.com>
2017-03-22 17:03:19 +0200

    first commit w2

---
+++ course1/projects/build.sbt
@@ -0,0 +1,6 @@
+name := "projects"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+    
---
+++ course1/week1-assignment/example/src/main/scala/example/ExampleApp.scala
@@ -0,0 +1,9 @@
+package example
+
+/**
+  * Created by p3700703 on 3/17/17.
+  */
+object ExampleApp extends App{
+  println("Hello, World")
+}
+
